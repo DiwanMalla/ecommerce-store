f1143c062bb65e8efcb8122590fe9f71d16ab7c7
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2023-12-28 01:25:47 +1100

    Remove image folder and copy img to public folder

--- src/components/promoCard.tsx
+++ src/components/promoCard.tsx
@@ -3,17 +3,14 @@ const promoCard = () => {
     <div id="outside wrapper" className="bg-gray-100">
       <div id="container" className="flex justify-center">
         <div id="firstColumn" className="w-[750px] h-[200px] m-3">
-          <img src="/images/entertainment.jpg" className="h-[450px]"></img>
+          <img src="entertainment.jpg" className="h-[450px]"></img>
         </div>
         <div id="secondColumn">
           <div id="first-row" className="m-3 w-[320px] ">
-            <img src="/images/leatherBag.jpg" className=""></img>
+            <img src="leatherBag.jpg" className=""></img>
           </div>
           <div id="first-row" className="m-3 w-[320px] bg-gray-400">
-            <img
-              src="images/BlenderCombo.jpg"
-              className="h-[230px] w-[320px]"
-            ></img>
+            <img src="BlenderCombo.jpg" className="h-[230px] w-[320px]"></img>
           </div>
         </div>
       </div>
