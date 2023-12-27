077fd4eacb4d2d5c8c43706cd44a3c3e76834c84
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2023-12-28 01:18:06 +1100

    Moved image to public folder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,23 @@ import NavBar from "./navBar";
 
 const Header = () => {
   return (
-    <div className="flex w-[1200px]">
-      <div className="w-[200px] h-[112px] border border-solid flex items-center justify-center">
-        <div className="">
-          <b>d1mart</b>
-        </div>
-      </div>
-      <div>
-        <div className="ml-3">
-          <div className="flex">
-            <Search />
-            <Cart />
+    <div className="flex justify-center">
+      <div className="flex ">
+        <div className="w-[200px] h-[112px] border border-solid flex items-center justify-center">
+          <div className="">
+            <b>d1mart</b>
           </div>
-          <NavBar />
         </div>
-        <hr className="mt-1 w-[930px]" />
+        <div>
+          <div className="ml-3">
+            <div className="flex">
+              <Search />
+              <Cart />
+            </div>
+            <NavBar />
+          </div>
+          <hr className="mt-1 w-[930px]" />
+        </div>
       </div>
     </div>
   );
