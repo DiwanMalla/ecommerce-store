3dcdc938fa5378fc8690fc0823d279f383a4058c
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-01-03 23:19:27 +1100

    Added FAQ feature in home page

--- src/components/frequentQuestion.tsx
+++ src/components/frequentQuestion.tsx
@@ -1,20 +1,35 @@
 import data from "./faqData";
+import { useState } from "react";
 const frequentQuestion = () => {
+  const [selected, setSelected] = useState([]);
+  const handleClicked = (selectedId: any) => {
+    if (selected.includes(selectedId)) {
+      setSelected(selected.filter((item) => item != selectedId));
+    } else {
+      setSelected([...selected, selectedId]);
+    }
+  };
   return (
-    <div className="bg-gray-600 ">
-      {data.map((data) => (
-        <div key={data.id} className="p-2 flex justify-center items-center">
-          <div
-            id="question"
-            className=" bg-white "
-            onClick={() => {
-              alert(`${data.id}`);
-            }}
-          >
-            {data.question}
+    <div className="bg-gray-600 flex justify-center">
+      <div id="wrapper" className="w-[400px]">
+        {data.map((data) => (
+          <div key={data.id} className="p-2">
+            <div
+              id="question"
+              className=" bg-white p-2 flex justify-between"
+              onClick={() => {
+                handleClicked(data.id);
+              }}
+            >
+              <h2>{data.question}</h2>
+              <span>{selected.includes(data.id) ? "-" : "+"}</span>
+            </div>
+            <div className="bg-gray-400">
+              <span>{selected.includes(data.id) && data.answer}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
