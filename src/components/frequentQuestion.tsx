4072255a7acf5a4e90dd7d477543a4265ead6390
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-01-04 18:34:59 +1100

    Added some style for FAQ

--- src/components/frequentQuestion.tsx
+++ src/components/frequentQuestion.tsx
@@ -11,13 +11,13 @@ const frequentQuestion = () => {
     }
   };
   return (
-    <div className="bg-gray-600 flex justify-center">
+    <div className="bg-gray-600 min-h-screen flex justify-center items-center py-8">
       <div id="wrapper" className="w-[400px]">
         {data.map((data) => (
           <div key={data.id} className="p-2">
             <div
               id="question"
-              className=" bg-white p-2 flex justify-between"
+              className=" bg-white rounded p-4 flex justify-between cursor-pointer"
               onClick={() => {
                 handleClicked(data.id);
               }}
@@ -25,9 +25,11 @@ const frequentQuestion = () => {
               <h2>{data.question}</h2>
               <span>{selected.includes(data.id) ? "-" : "+"}</span>
             </div>
-            <div className="bg-gray-400">
-              <span>{selected.includes(data.id) && data.answer}</span>
-            </div>
+            {selected.includes(data.id) && (
+              <div className="bg-gray-400 rounded-md mt-2 p-4">
+                <span>{data.answer}</span>
+              </div>
+            )}
           </div>
         ))}
       </div>
